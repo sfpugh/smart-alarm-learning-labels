3f0b89c5c75410dbcf22421a47db31e1f156a8af
eric.lu.py <eric.lu.py@gmail.com>
2020-05-19 00:16:44 -0400

    Add the pickle file for testing convenience. Add outlier detection functions using matrix profile algorithm. MP_WINDOW and MP_THRESHOLD needs tuning.

---
+++ spo2alarm_outlier_LFs.py
@@ -0,0 +1,83 @@
+from snorkel.labeling import labeling_function
+from snorkel.preprocess import preprocessor
+import pandas as pd
+import numpy as np
+import wfdb
+from matrixprofile import matrixProfile
+import matplotlib.pyplot as plt
+import time
+
+MIMIC3WDB = 'mimic3wdb/matched/'
+ABSTAIN = -1
+NOT_SUPPRESSIBLE = 0
+SUPPRESSIBLE = 1
+
+MP_WINDOW = 3000 # matrix profile window size
+MP_THRESHOLD = 10  # matrix profile threshold, decide outlier if > threshold
+
+np.seterr(divide='ignore', invalid='ignore')
+
+@preprocessor()
+def retrieve_spo2_mp(x):
+    rec_path = x.mimic3wdb_matched_ref.split('/')
+    pb_dir = MIMIC3WDB + rec_path[0] + '/' + rec_path[1]
+
+    try:
+        record = wfdb.rdrecord(rec_path[2], channel_names=['SpO2'], pb_dir=pb_dir)
+    except:
+        record = wfdb.rdrecord(rec_path[2], channel_names=['%SpO2'], pb_dir=pb_dir)
+
+    x.spo2 = record.p_signal[:, 0]
+    x.spo2_mp = matrixProfile.stomp(x.spo2, m=MP_WINDOW)
+    x.spo2_mp = np.nan_to_num(x.spo2_mp)
+    return x
+
+
+@labeling_function(pre=[retrieve_spo2_mp])
+def lf_outlier_120(x):
+    return NOT_SUPPRESSIBLE if np.any(x.spo2_mp[x.alarm_start - 120: x.alarm_start + 120] > MP_THRESHOLD) else ABSTAIN
+
+
+@labeling_function(pre=[retrieve_spo2_mp])
+def lf_outlier_100(x):
+    return NOT_SUPPRESSIBLE if np.any(x.spo2_mp[x.alarm_start - 100: x.alarm_start + 100] > MP_THRESHOLD) else ABSTAIN
+
+
+@labeling_function(pre=[retrieve_spo2_mp])
+def lf_outlier_90(x):
+    return NOT_SUPPRESSIBLE if np.any(x.spo2_mp[x.alarm_start - 90: x.alarm_start + 90] > MP_THRESHOLD) else ABSTAIN
+
+
+@labeling_function(pre=[retrieve_spo2_mp])
+def lf_outlier_60(x):
+    return NOT_SUPPRESSIBLE if np.any(x.spo2_mp[x.alarm_start - 60: x.alarm_start + 60] > MP_THRESHOLD) else ABSTAIN
+
+
+@labeling_function(pre=[retrieve_spo2_mp])
+def lf_outlier_30(x):
+    return NOT_SUPPRESSIBLE if np.any(x.spo2_mp[x.alarm_start - 30: x.alarm_start + 30] > MP_THRESHOLD) else ABSTAIN
+
+
+# testing on one time series to see if matrix profile works
+if __name__ == "__main__":
+    mimiciii_spo2alarms = pd.read_pickle('./spo2_alarms_df_v4.20.pkl')
+    x = mimiciii_spo2alarms.iloc[0]
+    s = time.time()
+    x = retrieve_spo2_mp(x)
+    print(x.spo2)
+    print(x.spo2_mp[0])
+    print('elapsed time: ' + str(time.time() - s) + 's')
+
+    # Append np.nan to Matrix profile to enable plotting against raw data
+    mp_adj = np.append(x.spo2_mp[0], np.zeros(MP_WINDOW - 1) + np.nan)
+
+    # Plot the signal data
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(20, 10))
+    ax1.plot(np.arange(len(x.spo2)), x.spo2, label="SpO2 Data")
+    ax1.set_ylabel('Signal', size=22)
+
+    # Plot the Matrix Profile
+    ax2.plot(np.arange(len(mp_adj)), mp_adj, label="Matrix Profile", color='red')
+    ax2.set_ylabel('Matrix Profile', size=22)
+
+    plt.show()
