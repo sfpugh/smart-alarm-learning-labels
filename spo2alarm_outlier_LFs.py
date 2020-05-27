Fixed mp computation errors caused by 1, points where moving std = 0 and 2, trailing np.nan in time series. Added labeling functions with thresholds tuned, such that outlier rate is 5%.

spo2alarm_outlier_LFs.py
@@ -12,37 +12,69 @@ ABSTAIN = -1
 NOT_SUPPRESSIBLE = 0
 SUPPRESSIBLE = 1
 
-# manually tune the parameters:
-MP_WINDOW = 500  # matrix profile window size
-MP_THRESHOLD = 10  # matrix profile threshold, decide outlier if > threshold
-
 np.seterr(divide='ignore', invalid='ignore')
 
 """The distance profile is computed by mass() and massStomp() subroutine, which normalize the profile value
     by dividing standard deviation of the moving window, causing error when the window has std = 0.
-    To avoid normalization, please edit mass() in matrixprofile.utils.py to:
+    To avoid normalization, please add this line to mass() and massStomp() in matrixprofile.utils.py:
     
-    def mass(query,ts, normalize=False):
-        #query_normalized = zNormalize(np.copy(query))
-        m = len(query)
-        q_mean = np.mean(query)
-        q_std = np.std(query)
-        mean, std = movmeanstd(ts,m)
-        dot = slidingDotProduct(query,ts)
-        #res = np.sqrt(2*m*(1-(dot-m*mean*q_mean)/(m*std*q_std)))
-        if normalize:
-            res = 2*m*(1-(dot-m*mean*q_mean)/(m*std*q_std))
-        else:
-            res = 2*m*(1-(dot-m*mean*q_mean)/m)
+    std[std == 0] = 1
     
-        return res
-        
-    And do the same to massStomp()
+    and add this to mass():
+    
+    if q_std == 0:
+        q_std = 1
 """
 
 
+# compute moving std
+def compute_std(ts, window):
+    ts_std = []
+    for i in range(len(ts) - window):
+        ts_std.append(np.std(ts[i: i+window-1]))
+    ts_std = np.array(ts_std)
+    ts_std_head = np.zeros(window // 2 - 1)
+    ts_std_tail = np.zeros(len(ts) - len(ts_std) - window // 2 + 1)
+    ts_std = np.concatenate([ts_std_head, ts_std, ts_std_tail])
+    return ts_std
+
+
+# retrieve interval centered at some time stamp
+def interval_centered_at(x, length, center):
+    start = max(0, center - length//2)
+    end = min(len(x)-1, center + length//2)
+    return x[start: end]
+
+
+# compute matrix profile at given window and threshold, return mp and percentage of outliers
+def compute_mp(ts, window, threshold):
+    # remove trailing nans of ts
+    i = len(ts) - 1
+    while np.isnan(ts[i]) and i >= 0:
+        i -= 1
+    ts = ts[0:i]
+    # compute mp by stamp
+    mp = np.array(matrixProfile.stomp(ts, m=window))[0]
+    # calibrate ts and mp, so mp value is assigned to the middle of that window
+    mp_head = np.zeros(window//2 - 1)
+    mp_tail = np.zeros(len(ts) - len(mp) - window//2 + 1)
+    mp = np.concatenate([mp_head, mp, mp_tail])
+    # remove error results due to zero std (make them 0 so they don't contribute to outliers)
+    ts_std = compute_std(ts, window=window)
+    count_zero_std = 0
+    for i in range(len(ts_std)):
+        if ts_std[i] == 0:
+            mp[i] = 0
+            count_zero_std += 1
+    # compute percentage of outliers, where head, tail and zero std points do not participate
+    outlier = mp[np.where(mp > threshold)]
+    outlier_percentage = len(outlier) / (len(mp) - len(mp_head) - len(mp_tail) - count_zero_std)
+    return mp, outlier_percentage
+
+
+# Same preprocessor retrieving SpO2 waveform
 @preprocessor()
-def retrieve_spo2_mp(x):
+def retrieve_spo2_waveform(x):
     rec_path = x.mimic3wdb_matched_ref.split('/')
     pb_dir = MIMIC3WDB + rec_path[0] + '/' + rec_path[1]
 
@@ -52,48 +84,97 @@ def retrieve_spo2_mp(x):
         record = wfdb.rdrecord(rec_path[2], channel_names=['%SpO2'], pb_dir=pb_dir)
 
     x.spo2 = record.p_signal[:, 0]
-    x.spo2_mp = matrixProfile.stomp(x.spo2, m=MP_WINDOW)
-    x.spo2_mp = np.nan_to_num(x.spo2_mp)
+
     return x
 
-
-@labeling_function(pre=[retrieve_spo2_mp])
-def lf_outlier_120(x):
-    return SUPPRESSIBLE if np.any(x.spo2_mp[x.alarm_start - 120: x.alarm_start + 120] > MP_THRESHOLD) else ABSTAIN
-
-
-@labeling_function(pre=[retrieve_spo2_mp])
-def lf_outlier_100(x):
-    return SUPPRESSIBLE if np.any(x.spo2_mp[x.alarm_start - 100: x.alarm_start + 100] > MP_THRESHOLD) else ABSTAIN
+# All labeling functions have thresholds tuned such that %outlier = 5%
+@labeling_function(pre=[retrieve_spo2_waveform])
+def lf_outlier_120_window(x):
+    x.spo2_mp, _ = compute_mp(x.spo2, window=120, threshold=8.4)
+    return SUPPRESSIBLE if np.any(interval_centered_at(x.spo2_mp, length=120, center=x.alarm_start) > 8.4) else ABSTAIN
 
 
-@labeling_function(pre=[retrieve_spo2_mp])
-def lf_outlier_90(x):
-    return SUPPRESSIBLE if np.any(x.spo2_mp[x.alarm_start - 90: x.alarm_start + 90] > MP_THRESHOLD) else ABSTAIN
+@labeling_function(pre=[retrieve_spo2_waveform])
+def lf_outlier_110_window(x):
+    x.spo2_mp, _ = compute_mp(x.spo2, window=110, threshold=7.8)
+    return SUPPRESSIBLE if np.any(interval_centered_at(x.spo2_mp, length=110, center=x.alarm_start) > 7.8) else ABSTAIN
 
 
-@labeling_function(pre=[retrieve_spo2_mp])
-def lf_outlier_60(x):
-    return SUPPRESSIBLE if np.any(x.spo2_mp[x.alarm_start - 60: x.alarm_start + 60] > MP_THRESHOLD) else ABSTAIN
+@labeling_function(pre=[retrieve_spo2_waveform])
+def lf_outlier_100_window(x):
+    x.spo2_mp, _ = compute_mp(x.spo2, window=100, threshold=7.2)
+    return SUPPRESSIBLE if np.any(interval_centered_at(x.spo2_mp, length=100, center=x.alarm_start) > 7.2) else ABSTAIN
 
 
-@labeling_function(pre=[retrieve_spo2_mp])
-def lf_outlier_30(x):
-    return SUPPRESSIBLE if np.any(x.spo2_mp[x.alarm_start - 30: x.alarm_start + 30] > MP_THRESHOLD) else ABSTAIN
+@labeling_function(pre=[retrieve_spo2_waveform])
+def lf_outlier_90_window(x):
+    x.spo2_mp, _ = compute_mp(x.spo2, window=90, threshold=6.6)
+    return SUPPRESSIBLE if np.any(interval_centered_at(x.spo2_mp, length=90, center=x.alarm_start) > 6.6) else ABSTAIN
 
 
-# testing on one time series to see if matrix profile works
+@labeling_function(pre=[retrieve_spo2_waveform])
+def lf_outlier_80_window(x):
+    x.spo2_mp, _ = compute_mp(x.spo2, window=80, threshold=6)
+    return SUPPRESSIBLE if np.any(interval_centered_at(x.spo2_mp, length=80, center=x.alarm_start) > 6) else ABSTAIN
+
+
+@labeling_function(pre=[retrieve_spo2_waveform])
+def lf_outlier_70_window(x):
+    x.spo2_mp, _ = compute_mp(x.spo2, window=70, threshold=5.3)
+    return SUPPRESSIBLE if np.any(interval_centered_at(x.spo2_mp, length=70, center=x.alarm_start) > 5.3) else ABSTAIN
+
+
+@labeling_function(pre=[retrieve_spo2_waveform])
+def lf_outlier_60_window(x):
+    x.spo2_mp, _ = compute_mp(x.spo2, window=60, threshold=4.6)
+    return SUPPRESSIBLE if np.any(interval_centered_at(x.spo2_mp, length=60, center=x.alarm_start) > 4.6) else ABSTAIN
+
+
+@labeling_function(pre=[retrieve_spo2_waveform])
+def lf_outlier_50_window(x):
+    x.spo2_mp, _ = compute_mp(x.spo2, window=50, threshold=3.8)
+    return SUPPRESSIBLE if np.any(interval_centered_at(x.spo2_mp, length=50, center=x.alarm_start) > 3.8) else ABSTAIN
+
+
+@labeling_function(pre=[retrieve_spo2_waveform])
+def lf_outlier_40_window(x):
+    x.spo2_mp, _ = compute_mp(x.spo2, window=40, threshold=2.9)
+    return SUPPRESSIBLE if np.any(interval_centered_at(x.spo2_mp, length=40, center=x.alarm_start) > 2.9) else ABSTAIN
+
+
+@labeling_function(pre=[retrieve_spo2_waveform])
+def lf_outlier_30_window(x):
+    x.spo2_mp, _ = compute_mp(x.spo2, window=30, threshold=2.1)
+    return SUPPRESSIBLE if np.any(interval_centered_at(x.spo2_mp, length=30, center=x.alarm_start) > 2.1) else ABSTAIN
+
+
+@labeling_function(pre=[retrieve_spo2_waveform])
+def lf_outlier_20_window(x):
+    x.spo2_mp, _ = compute_mp(x.spo2, window=20, threshold=1)
+    return SUPPRESSIBLE if np.any(interval_centered_at(x.spo2_mp, length=20, center=x.alarm_start) > 1) else ABSTAIN
+
+
+# one function with very long window
+@labeling_function(pre=[retrieve_spo2_waveform])
+def lf_outlier_3000_window(x):
+    x.spo2_mp, _ = compute_mp(x.spo2, window=3000, threshold=72)
+    return SUPPRESSIBLE if np.any(interval_centered_at(x.spo2_mp, length=3000, center=x.alarm_start) > 72) else ABSTAIN
+
+
+# test the implementation on one spo2 time series
 if __name__ == "__main__":
+    w = 120
+    t = 8.4
     mimiciii_spo2alarms = pd.read_pickle('./spo2_alarms_df_v4.20.pkl')
     x = mimiciii_spo2alarms.iloc[0]
-    s = time.time()
-    x = retrieve_spo2_mp(x)
-    print(x.spo2)
-    print(x.spo2_mp[0])
-    print('elapsed time: ' + str(time.time() - s) + 's')
+    x = retrieve_spo2_waveform(x)
 
-    # Append np.nan to Matrix profile to enable plotting against raw data
-    mp_adj = np.append(x.spo2_mp[0], np.zeros(MP_WINDOW - 1) + np.nan)
+    # Compute matrix profile
+    x.spo2_mp, outlier_percent = compute_mp(x.spo2, window=w, threshold=t)
+    s = time.time()
+    print('elapsed time: ' + str(time.time() - s) + 's')
+    print("window = " + str(w) + " threshold = " + str(t) + " len = " + str(len(x.spo2)) + " %outlier = " + str(
+        outlier_percent))
 
     # Plot the signal data
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(20, 10))
@@ -101,7 +182,9 @@ if __name__ == "__main__":
     ax1.set_ylabel('Signal', size=22)
 
     # Plot the Matrix Profile
-    ax2.plot(np.arange(len(mp_adj)), mp_adj, label="Matrix Profile", color='red')
+    ax2.plot(range(100))
+    ax2.plot(np.arange(len(x.spo2_mp)), [t] * len(x.spo2_mp), color='black')
+    ax2.plot(np.arange(len(x.spo2_mp)), x.spo2_mp, label="Matrix Profile", color='red')
     ax2.set_ylabel('Matrix Profile', size=22)
 
     plt.show()