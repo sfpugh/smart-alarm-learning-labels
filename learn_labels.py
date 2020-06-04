e9101c06141e1fbe3973914182d628a416654e7e
Sydney <sydney@pugh.us.com>
2020-06-04 06:39:52 -0400

    beginning to add matrix profile lfs, currently cannot install matrixprofile module on Ash

--- learn_labels.py
+++ learn_labels.py
@@ -4,8 +4,10 @@ from snorkel.labeling import labeling_function, LFAnalysis
 from sklearn.metrics import confusion_matrix
 from read_chop_data import read_alarms, read_vitals
 from datetime import datetime, timedelta
+from matrixprofile import matrixProfile
 import pandas as pd
 import numpy as np
+
 
 ## Global vars
 # Label mappings
@@ -13,11 +15,14 @@ ABSTAIN = -1
 NOT_SUPPRESSIBLE = 0
 SUPPRESSIBLE = 1
 
-AGE_FACTOR = 1
-
 # Data dataframes
 alarms_df = read_alarms()
 vitals_dfs = read_vitals()
+age_factors_df = pd.DataFrame({'pt_age_group':[1,2,3,4], 
+                                    'pt_age_group_L':['< 1 month','1-< 2 month','2-< 6 month','6 months and older'], 
+                                    'hr_age_factor':[1,1,1,1], 
+                                    'rr_age_factor':[1,1,1,1]}, 
+                                    index=[1,2,3,4]) 
 
 
 @labeling_function()
@@ -47,13 +52,11 @@ def lf_short_alarm_5s(x):
     return SUPPRESSIBLE if x.duration <= 5 else ABSTAIN
 
 
-def max_recovery(x, spo2_data):
+def max_recovery(data):
     r = []
-    end = spo2_data.index[-1]
-    print(end)
-
-    for (i, row) in spo2_data.iterrows():
-        print(i)
+    idxs = spo2.index
+    print(idxs)
+    print(idxs[1])
 
     return max(r)
 
@@ -64,7 +67,9 @@ def lf_immediate_recovery_10s(x):
     If SpO2 level increases/recovers by more than 20 percentage points within 
     10 seconds of alarm start then the alarm is suppressible, otherwise abstain
     """
-    return ABSTAIN
+    v_df = vitals_dfs[x.pt_id]
+    spo2_subrange = v_df.loc[x.alarm_datetime:x.alarm_datetime+timedelta(seconds=10), ['SPO2-%']]
+    return SUPPRESSIBLE if max_recovery(spo2_subrange) > 20 else ABSTAIN
 
 
 @labeling_function()
@@ -73,7 +78,9 @@ def lf_immediate_recovery_15s(x):
     If SpO2 level increases/recovers by more than 30 percentage points within 
     15 seconds of alarm start then the alarm is suppressible, otherwise abstain
     """
-    return ABSTAIN
+    v_df = vitals_dfs[x.pt_id]
+    spo2_subrange = v_df.loc[x.alarm_datetime:x.alarm_datetime+timedelta(seconds=15), ['SPO2-%']]
+    return SUPPRESSIBLE if max_recovery(spo2_subrange) > 30 else ABSTAIN
 
 
 @labeling_function()
@@ -188,7 +195,8 @@ def lf_hr_below50_over120s(x):
     """
     v_df = vitals_dfs[x.pt_id]
     hr_subrange = v_df.loc[x.alarm_datetime:x.alarm_datetime+timedelta(seconds=121), ['HR']]
-    return NOT_SUPPRESSIBLE if np.all( (40*AGE_FACTOR < hr_subrange) & (hr_subrange <= 50*AGE_FACTOR) ) else ABSTAIN
+    age_factor = age_factors_df.hr_age_factor[x.pt_age_group]
+    return NOT_SUPPRESSIBLE if np.all( (40*age_factor < hr_subrange) & (hr_subrange <= 50*age_factor) ) else ABSTAIN
 
 
 @labeling_function()
@@ -199,7 +207,8 @@ def lf_hr_below40_over60s(x):
     """
     v_df = vitals_dfs[x.pt_id]
     hr_subrange = v_df.loc[x.alarm_datetime:x.alarm_datetime+timedelta(seconds=61), ['HR']]
-    return NOT_SUPPRESSIBLE if np.all( (30*AGE_FACTOR < hr_subrange) & (hr_subrange <= 40*AGE_FACTOR) ) else ABSTAIN
+    age_factor = age_factors_df.hr_age_factor[x.pt_age_group]
+    return NOT_SUPPRESSIBLE if np.all( (30*age_factor < hr_subrange) & (hr_subrange <= 40*age_factor) ) else ABSTAIN
 
 
 @labeling_function()
@@ -210,18 +219,20 @@ def lf_hr_below30(x):
     """
     v_df = vitals_dfs[x.pt_id]
     hr_subrange = v_df.loc[x.alarm_datetime:x.alarm_datetime+timedelta(seconds=x.duration), ['HR']]
-    return NOT_SUPPRESSIBLE if np.any( hr_subrange <= 30 * AGE_FACTOR ) else ABSTAIN
+    age_factor = age_factors_df.hr_age_factor[x.pt_age_group]
+    return NOT_SUPPRESSIBLE if np.any( hr_subrange <= 30*age_factor ) else ABSTAIN
 
 
 @labeling_function()
 def lf_rr_below50_over120s(x):
     """
-    If HR below 50 * age factor for over 120 seconds since alarm start then the alarm is not suppressible, 
+    If RR below 50 * age factor for over 120 seconds since alarm start then the alarm is not suppressible, 
     otherwise abstain
     """
     v_df = vitals_dfs[x.pt_id]
-    hr_subrange = v_df.loc[x.alarm_datetime:x.alarm_datetime+timedelta(seconds=121), ['HR']]
-    return NOT_SUPPRESSIBLE if np.all( (40*AGE_FACTOR < hr_subrange) & (hr_subrange <= 50*AGE_FACTOR) ) else ABSTAIN
+    rr_subrange = v_df.loc[x.alarm_datetime:x.alarm_datetime+timedelta(seconds=121), ['RESP']]
+    age_factor = age_factors_df.rr_age_factor[x.pt_age_group]
+    return NOT_SUPPRESSIBLE if np.all( (40*age_factor < rr_subrange) & (rr_subrange <= 50*age_factor) ) else ABSTAIN
 
 
 @labeling_function()
@@ -231,8 +242,9 @@ def lf_rr_below40_over60s(x):
     otherwise abstain
     """
     v_df = vitals_dfs[x.pt_id]
-    hr_subrange = v_df.loc[x.alarm_datetime:x.alarm_datetime+timedelta(seconds=61), ['RESP']]
-    return NOT_SUPPRESSIBLE if np.all( (30*AGE_FACTOR < hr_subrange) & (hr_subrange <= 40*AGE_FACTOR) ) else ABSTAIN
+    rr_subrange = v_df.loc[x.alarm_datetime:x.alarm_datetime+timedelta(seconds=61), ['RESP']]
+    age_factor = age_factors_df.rr_age_factor[x.pt_age_group]
+    return NOT_SUPPRESSIBLE if np.all( (30*age_factor < rr_subrange) & (rr_subrange <= 40*age_factor) ) else ABSTAIN
 
 
 @labeling_function()
@@ -242,8 +254,9 @@ def lf_rr_below30(x):
     otherwise abstain
     """
     v_df = vitals_dfs[x.pt_id]
-    hr_subrange = v_df.loc[x.alarm_datetime:x.alarm_datetime+timedelta(seconds=x.duration), ['RESP']]
-    return NOT_SUPPRESSIBLE if np.any( rr_subrange <= 30 * AGE_FACTOR ) else ABSTAIN
+    rr_subrange = v_df.loc[x.alarm_datetime:x.alarm_datetime+timedelta(seconds=x.duration), ['RESP']]
+    age_factor = age_factors_df.rr_age_factor[x.pt_age_group]
+    return NOT_SUPPRESSIBLE if np.any( rr_subrange <= 30*age_factor ) else ABSTAIN
 
 
 def repeat_alarms(x, t):
@@ -270,21 +283,70 @@ def repeat_alarms(x, t):
 
 
 @labeling_function()
-def lf_repeat_alarms_5m(x):
+def lf_repeat_alarms_15s(x):
     """
-    If there exists other alarms within 5 minutes of the current alarm then the alarm is
+    If there exists other alarms within 15 seconds of the current alarm then the alarm is
     not suppressible, otherwise abstain
     """
-    return repeat_alarms(x, 300) 
+    return repeat_alarms(x, 15) 
 
 
 @labeling_function()
-def lf_repeat_alarms_1m(x):
+def lf_repeat_alarms_30s(x):
     """
-    If there exists other alarms within 1 minute of the current alarm then the alarm is
+    If there exists other alarms within 30 seconds of the current alarm then the alarm is
+    not suppressible, otherwise abstain
+    """
+    return repeat_alarms(x, 30) 
+
+
+@labeling_function()
+def lf_repeat_alarms_60s(x):
+    """
+    If there exists other alarms within 60 seconds of the current alarm then the alarm is
     not suppressible, otherwise abstain
     """
     return repeat_alarms(x, 60) 
+
+
+# retrieve interval centered at some time stamp
+def interval_centered_at(x, length, center):
+    start = max(0, center - length//2)
+    end = min(len(x)-1, center + length//2)
+    return x[start: end]
+
+
+# compute matrix profile at given window and threshold, return mp and percentage of outliers
+def compute_mp(ts, window, threshold):
+    # remove trailing nans of ts
+    i = len(ts) - 1
+    while np.isnan(ts[i]) and i >= 0:
+        i -= 1
+    ts = ts[0:i]
+    # compute mp by stamp
+    mp = np.array(matrixProfile.stomp(ts, m=window))[0]
+    # calibrate ts and mp, so mp value is assigned to the middle of that window
+    mp_head = np.zeros(window//2 - 1)
+    mp_tail = np.zeros(len(ts) - len(mp) - window//2 + 1)
+    mp = np.concatenate([mp_head, mp, mp_tail])
+    # remove error results due to zero std (make them 0 so they don't contribute to outliers)
+    ts_std = compute_std(ts, window=window)
+    count_zero_std = 0
+    for i in range(len(ts_std)):
+        if ts_std[i] == 0:
+            mp[i] = 0
+            count_zero_std += 1
+    # compute percentage of outliers, where head, tail and zero std points do not participate
+    outlier = mp[np.where(mp > threshold)]
+    outlier_percentage = len(outlier) / (len(mp) - len(mp_head) - len(mp_tail) - count_zero_std)
+    return mp, outlier_percentage
+
+
+@labeling_function()
+def lf_outlier_spo2_110(x):
+    v_df = vitals_dfs[x.pt_id]
+    spo2_mp, _ = compute_mp(v_df['SPO2-%'], window=110, threshold=7.8)
+    return SUPPRESSIBLE if np.any(interval_centered_at(spo2_mp, length=110, center=x.alarm_datetime) > 7.8) else ABSTAIN
 
 
 def pred_threshold(p, thres):
@@ -302,11 +364,9 @@ def pred_threshold(p, thres):
     if p[SUPPRESSIBLE] > p[NOT_SUPPRESSIBLE] and p[SUPPRESSIBLE] >= thres:
         return SUPPRESSIBLE
     elif p[NOT_SUPPRESSIBLE] > p[SUPPRESSIBLE]:
-    #elif p[NOT_SUPPRESSIBLE] > p[SUPPRESSIBLE] or p[NOT_SUPPRESSIBLE] > 0.5:
         return NOT_SUPPRESSIBLE
     else:
         return ABSTAIN
-# end pred_threshold
 
 
 def main():
@@ -316,13 +376,13 @@ def main():
             lf_hr_tech_err_20, lf_hr_tech_err_30,
             lf_spo2_below85_over120s, lf_spo2_below80_over100s, lf_spo2_below70_over90s, lf_spo2_below60_over60s, lf_spo2_below50_over30s,
             lf_hr_below50_over120s, lf_hr_below40_over60s, lf_hr_below30,
-            lf_rr_below50_over120s, lf_rr_below40_over60s, lf_rr_below30]
-            #lf_repeat_alarms_5m, lf_repeat_alarms_1m]
+            lf_rr_below50_over120s, lf_rr_below40_over60s, lf_rr_below30,
+            lf_repeat_alarms_15s, lf_repeat_alarms_30s, lf_repeat_alarms_60s]
 
     print('Applying LFs...\n')
     applier = PandasParallelLFApplier(lfs)
     L_train = applier.apply(alarms_df, n_parallel=10, scheduler='threads', fault_tolerant=True)
-    print( LFAnalysis(L_train,lfs).lf_summary(), '\n' )
+    print(LFAnalysis(L_train, lfs).lf_summary(Y=alarms_df.true_label.to_numpy()), '\n')
 
 
     ## Train a label model
@@ -332,35 +392,29 @@ def main():
 
 
     ## Evaluate label model accuracy
-
     # original prediction method
     pred = label_model.predict(L_train)
     print('Confusion Matrix (original): ')
-    print( confusion_matrix(alarms_df.true_label, pred) )
+    print(confusion_matrix(alarms_df.true_label, pred))
 
     # predict with threshold
     pred_probs = label_model.predict_proba(L_train)
 
-    for thres in [0.7,0.8,0.9]:
+    for thres in [0.7,0.8,0.9,0.95,0.99]:
         pred = np.apply_along_axis(pred_threshold, 1, pred_probs, thres)
         print('Confusion Matrix (threshold=' + str(thres) + '):')
         print( confusion_matrix(alarms_df.true_label, pred) )
-# end main
 
 
-def test():
+def test_lf():
     x = alarms_df.iloc[194]
     print(x)
 
-    v_df = vitals_dfs[x.pt_id]
-    spo2_subrange = v_df.loc[x.alarm_datetime:x.alarm_datetime+timedelta(seconds=10), ['SPO2-%']]
-    #print(spo2_subrange)
-
-    max_recovery(x, spo2_subrange)
+    lf_outlier_spo2_110(x)
     
     exit(0)
 
 
 if __name__ == '__main__':
-    #test()
-    main()
+    #main()
+    test_lf()
