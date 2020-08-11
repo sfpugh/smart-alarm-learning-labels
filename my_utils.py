de8d242a5080d7b94c59769f0212755acb8fa22c
Sydney <sfpugh@seas.upenn.edu>
2020-08-11 16:22:34 -0400

    adding original experiment with regular label model

--- my_utils.py
+++ my_utils.py
@@ -18,3 +18,22 @@ def threshold_predict(p, threshold):
         return NOT_SUPPRESSIBLE
     else:
         return ABSTAIN
+
+def threshold_suppressible_predict(p, threshold):
+    """
+    Threshold the prediction of suppressible alarms, i.e., in order to
+    predict suppressible the P(SUPPRESSIBLE | LFs) must be the majority
+    and greater than the threshold
+    
+    Parameters:
+        p - array of probabilities for each label 
+        threshold - threshold of probability to make prediction
+    Returns:
+        int - prediction of supprressible or not suppressible
+    """
+    if p[SUPPRESSIBLE] > p[NOT_SUPPRESSIBLE] and p[SUPPRESSIBLE] > threshold:
+        return SUPPRESSIBLE
+    elif p[NOT_SUPPRESSIBLE] > p[SUPPRESSIBLE]:
+        return NOT_SUPPRESSIBLE
+    else:
+        return ABSTAIN
