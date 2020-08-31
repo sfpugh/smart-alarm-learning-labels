f322e664b32e13f7712973527de6eda9bab8a9bb
Sydney <sfpugh@seas.upenn.edu>
2020-08-31 06:20:01 -0400

    updated and final

---
+++ exp/E3-InformedLabelModel.py
@@ -0,0 +1,77 @@
+import sys
+import logging
+import numpy as np
+from sklearn.model_selection import KFold, train_test_split
+from Our_Monitors.CD_Monitor import Informed_LabelModel
+from Our_Monitors.CDGA_Monitor_fast import CDGAM_fast as CDGAM
+from snorkel.analysis import Scorer
+
+import my_utils
+import importlib
+importlib.reload(my_utils)
+from my_utils import predict_at_abstain_rate
+
+
+# Extract parameters from arguments
+n_epochs = int(sys.argv[1])
+lr = float(sys.argv[2])
+sig = float(sys.argv[3])
+policy = str(sys.argv[4])
+abstain_rate = float(sys.argv[5])   # if < 0 then no abstain rate requested
+
+# Other parameters
+n_folds = 5
+
+# Extract relevant data
+L_data = np.copy(L_alarms[:,:57])
+Y_data = np.copy(Y_alarms)
+
+# Set up Scorer
+my_metrics = {"abstain rate": lambda golds, preds, probs: np.sum(preds == ABSTAIN) / len(preds)}
+scorer = Scorer(metrics=["accuracy","f1"], custom_metric_funcs=my_metrics)
+
+# 5-fold cross validation
+all_scores = []
+kf = KFold(n_splits=n_folds, shuffle=True)
+
+for i, (train_dev_idx, test_idx) in enumerate(kf.split(L_data)):
+    train_idx, dev_idx = train_test_split(train_dev_idx, test_size=0.25, shuffle=True)
+    # Define train dataset
+    L_train = L_data[train_idx]
+    Y_train = Y_data[train_idx]
+    # Define development dataset
+    L_dev = L_data[dev_idx]
+    Y_dev = Y_data[dev_idx]
+    # Define test dataset
+    L_test = L_data[test_idx]
+    Y_test = Y_data[test_idx]
+
+    # Learn dependencies
+    deps = CDGAM(L_dev, k=2, sig=sig, policy=policy, verbose=False, return_more_info=False)
+
+    # Evaluate a dependency-informed Snorkel model
+    il_model = Informed_LabelModel(deps, cardinality=2, verbose=False)
+    il_model.fit(L_train, n_epochs=n_epochs, lr=lr)
+
+    try:
+        if abstain_rate < 0:
+            Y_pred = il_model.predict(L_test, tie_break_policy="abstain")
+        else:
+            Y_prob = l_model.predict_proba(L_test)
+            Y_pred = predict_at_abstain_rate(Y_prob, abstain_rate)
+
+        scores = scorer.score(Y_test, preds=Y_pred)
+        scores["num deps"] = len(deps)
+        all_scores.append(scores)
+    except Exception as e:
+        print(e)
+        continue
+    
+    # Logging
+    print("Iteration " + str(i+1) + ":",  scores)
+
+print("-- SUMMARY --")
+print("accuracy: AVG {:.3f}, STD {:.3f}".format(np.mean([s["accuracy"] for s in all_scores]), np.std([s["accuracy"] for s in all_scores])))
+print("f1: AVG {:.3f}, STD {:.3f}".format(np.mean([s["f1"] for s in all_scores]), np.std([s["f1"] for s in all_scores])))
+print("abstain rate: AVG {:.3f}, STD {:.3f}".format(np.mean([s["abstain rate"] for s in all_scores]), np.std([s["abstain rate"] for s in all_scores])))
+print("num deps: AVG {:.3f}, STD {:.3f}".format(np.mean([s["num deps"] for s in all_scores]), np.std([s["num deps"] for s in all_scores])))
--- exp/E6-RecursiveClustering.py
+++ exp/E6-RecursiveClustering.py
@@ -1,12 +1,18 @@
 import sys
 import logging
+import threading
 import numpy as np
 from sklearn.model_selection import train_test_split
 from Our_Monitors.CD_Monitor import Informed_LabelModel
 #from Our_Monitors.CDGA_Monitor import CDGAM
 from Our_Monitors.CDGA_Monitor_fast import CDGAM_fast as CDGAM
 from snorkel.analysis import Scorer
+
+import my_utils
+import importlib
+importlib.reload(my_utils)
 from my_utils import predict_at_abstain_rate
+
 import matplotlib.pyplot as plt
 
 
@@ -16,8 +22,7 @@ n_iters = int(sys.argv[2])              # N
 n_subsets = int(sys.argv[3])            # J
 subset_size = int(sys.argv[4])          # K
 with_replacement = bool(sys.argv[5])
-abstain_rate = float(sys.argv[6])
-
+abstain_rate = float(sys.argv[6])       # if < 0 then no abstain rate requested
 
 # Other parameters
 n_epochs = 100
@@ -25,31 +30,25 @@ lr = 0.01
 sig = 0.05
 policy = "new"
 
-
 # Copy data from notebook
-L_data = np.copy(L_alarms[:,:57])
-Y_data = np.copy(Y_alarms)
-
+L_data_global = np.copy(L_alarms[:,:57])
+Y_data_global = np.copy(Y_alarms)
 
 # Set up Scorer
 my_metrics = {"abstain rate": lambda golds, preds, probs: np.sum(preds == ABSTAIN) / len(preds)}
 scorer = Scorer(metrics=["accuracy","f1"], custom_metric_funcs=my_metrics)
 
+# Define the experiment
+results_mtx = np.empty((n_exps,4,n_iters), dtype=float)
+results_mtx[:] = np.nan
 
-# Run the experiment
-results_mtx = np.zeros((n_exps,4,n_iters), dtype=float)
-
-for exp in range(n_exps):
-    logging.info("- Experiment " + str(exp+1) + "-")
-
+def thread_experiment(exp, L_data, Y_data):
     for iter in range(n_iters):
-        print("-- Iteration " + str(iter+1) + "--")
-
         # Randomly sample J sets of K LFs
         subsets = np.random.choice(L_data.shape[1], size=(n_subsets,subset_size), replace=with_replacement)
 
-        # Define a new LF for each of the J sets as the prediction of a dependency-informed Snorkel model with K LFs
-        L_train, L_dev = train_test_split(L_data, test_size=0.2, shuffle=True)   
+        # Define a new LF for each of the J sets as the prediction of a dependency-informed Snorkel model with the K LFs
+        L_train, L_dev = train_test_split(L_data, test_size=0.2, shuffle=True)
 
         new_L_data = np.zeros((L_data.shape[0],n_subsets))         
 
@@ -65,29 +64,45 @@ for exp in range(n_exps):
         L_train, L_dev, Y_train, _ = train_test_split(L_data, Y_data, test_size=0.2, shuffle=True)
 
         deps = CDGAM(L_dev, k=2, sig=sig, policy=policy, verbose=False, return_more_info=False)
-
         il_model = Informed_LabelModel(deps, cardinality=2)
-        il_model.fit(L_train, n_epochs=n_epochs, lr=lr)
+        il_model.fit(L_train, n_epochs=n_epochs, lr=lr)            
 
         # Evaluate model at specified abstain rate
-        Y_pred = il_model.predict(L_train)
-        #Y_prob = il_model.predict_proba(L_train)
-        #Y_pred = predict_at_abstain_rate(Y_prob, abstain_rate)
-        iter_scores = scorer.score(Y_train, preds=Y_pred)
-        iter_scores["num deps"] = len(deps)
+        try:
+            if abstain_rate < 0:
+                Y_pred = il_model.predict(L_train)
+            else:
+                Y_prob = il_model.predict_proba(L_train)
+                Y_pred = predict_at_abstain_rate(Y_prob, abstain_rate)
 
-        # Logging
-        logging.info(iter_scores)
-        results_mtx[exp,:,iter] = list(iter_scores.values())
-# end experiment
+            iter_scores = scorer.score(Y_train, preds=Y_pred)
+            iter_scores["num deps"] = len(deps)
+            results_mtx[exp,:,iter] = list(iter_scores.values())
+        except Exception as e:
+            print("Thread {}: Iter {}: {}".format(exp,iter,e))
+            continue
+
+
+# Run the experiment several times
+threads = []
+for exp in range(n_exps):
+    print("Main: create and start experiment thread {}".format(exp)) 
+    x = threading.Thread(target=thread_experiment, args=(exp, L_data_global, Y_data_global))
+    threads.append(x)
+    x.start()
+
+for exp, thread in enumerate(threads):
+    thread.join()
+    print("Main: experiment thread {} done".format(exp))
 
 
 # Plotting
 fig, (ax1, ax2, ax3) = plt.subplots(3, figsize=(4,10))
-fig.suptitle("E6 results \n (N=" + str(n_iters) 
+fig.suptitle("E6 results (exps=" + str(n_exps)
+                        + ", N=" + str(n_iters) 
                         + ", J=" + str(n_subsets) 
                         + ", K=" + str(subset_size) 
-                        + ", replacement=" + str(with_replacement) 
+                        + ", \nreplacement=" + str(with_replacement) 
                         + ", abstain=" + str(abstain_rate) 
                         + ")", y=0.95) 
 # Accuracy
@@ -102,3 +117,5 @@ ax2.set_ylim([0.0,1.0])
 ax3.boxplot(results_mtx[:,3,::1], positions=range(1,n_iters+1))
 ax3.set(xlabel="Iteration", ylabel="Num Dependencies (CDGAM)")
 plt.show()
+
+print("abstain rate: AVG {:.3f}, STD {:.3f} ".format(np.nanmean(results_mtx[:,2,:]), np.nanstd(results_mtx[:,2,:]))) 
