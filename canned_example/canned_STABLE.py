4f6df36ddfa2facd3bc4d17959dd76bdfa56a7c0
Sydney <sfpugh@seas.upenn.edu>
2020-10-12 11:07:49 -0400

    adding canned example

---
+++ canned_example/canned_STABLE.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Canned Example: sign(x)
+# This notebook defines our canned example which defines several labeling functions that model the sign function. (See Overleaf document for full definition of this example.)
+
+# In[1]:
+
+
+import numpy as np
+from snorkel.labeling import labeling_function, LFApplier, LFAnalysis
+from snorkel.labeling.model.label_model import LabelModel
+
+
+# In[2]:
+
+
+# Parameters for the analysis
+N_LFS = 10
+N_DPS = 10000
+
+SEED_RNG = False
+SEED = 12345
+
+
+# In[3]:
+
+
+# Defining labels for sake of clarity
+ABSTAIN = 0
+NEGATIVE = -1
+POSITIVE = 1
+
+
+# In[4]:
+
+
+# Seed numpy.random for reproducible results
+if SEED_RNG:
+    np.random.seed(SEED)
+
+
+# ## Define the Goal Model
+
+# In[5]:
+
+
+def f_star(x):
+    return 1 if x == 0 else np.sign(x)
+
+
+# ## Define the Dataset
+
+# In[6]:
+
+
+X_data = np.random.uniform(-1, 1, N_DPS)
+
+f_star_vectorized = np.vectorize(f_star)
+Y_data = f_star_vectorized(X_data)
+
+
+# ## Define Labeling Functions
+
+# In[7]:
+
+
+def f(p_f, p_A, x):    
+    # Draw from Bernoulli distribution
+    z_f = np.random.binomial(1, p_f, 1)[0]
+    z_A = np.random.binomial(1, p_A, 1)[0]
+    
+    return int( (f_star(x) * z_f - f_star(x) * (1 - z_f)) * (1 - z_A) )
+
+
+# In[8]:
+
+
+lfs = []
+
+# Generate several labeling functions for different values of p_f and p_A
+for i in range(N_LFS):
+    p_f = np.random.rand(1)[0]
+    p_A = np.random.rand(1)[0]
+    
+    @labeling_function(name="lf{}({:.4f},{:.4f})".format(i, p_f, p_A))
+    def lf(x):
+        return f(p_f, p_A, x)
+    
+    lfs.append(lf)
+
+
+# ## Apply Labeling Functions to the Data
+
+# In[9]:
+
+
+applier = LFApplier(lfs)
+L_data = applier.apply(X_data, progress_bar=True)
+
+
+# In[10]:
+
+
+LFAnalysis(L_data, lfs=lfs).lf_summary(Y_data)
+
+
+# ## Experiments
+
+# In[ ]:
+
+
+
+
